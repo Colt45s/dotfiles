b9d46ea5c9b16ad5da9682dc8b27ce1ef4f32bc8
Colt45s <kzk0693@gmail.com>
2024-06-04 18:23:38 +0900

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,24 +1,167 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 require("status")
 
--- This table will hold the configuration.
-local config = {
+local LEFT_ARROW = utf8.char(0xff0b3)
+-- The filled in variant of the < symbol
+local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+-- The filled in variant of the > symbol
+local SOLID_RIGHT_ARROW = utf8.char(0xe0b0)
+
+local COL_BG = "#24283b"
+local COL_BG_ALT = "#292e42"
+local COL_FG = "#c0caf5"
+local COL_FG_ALT = "#a9b1d6"
+local COL_ACCENT = "#3b4261"
+
+local function strip_home_name(text)
+	local username = os.getenv("USER")
+	local clean_text = text:gsub("/home/" .. username, "~")
+	return clean_text
+end
+
+wezterm.on("format-window-title", function(tab, pane, tabs, panes, config)
+	local zoomed = ""
+	if tab.active_pane.is_zoomed then
+		zoomed = "[Z] "
+	end
+
+	local index = ""
+	if #tabs > 1 then
+		index = string.format("[%d/%d] ", tab.tab_index + 1, #tabs)
+	end
+
+	local clean_title = strip_home_name(tab.active_pane.title)
+	return zoomed .. index .. clean_title
+end)
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	-- edge icon
+	local edge_background = COL_BG
+	-- inactive tab
+	local background = COL_BG_ALT
+	local foreground = COL_FG
+
+	if tab.is_active then
+		background = COL_FG_ALT
+		foreground = COL_BG
+	elseif hover then
+		background = COL_ACCENT
+		foreground = COL_FG
+	end
+
+	local edge_foreground = background
+	local clean_title = strip_home_name(tab.active_pane.title)
+
+	return {
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_LEFT_ARROW },
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = clean_title },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_RIGHT_ARROW },
+	}
+end)
+
+return {
+	color_scheme = "tokyonight",
+	default_cursor_style = "BlinkingBar",
+	font_size = 18.0,
 	font = wezterm.font_with_fallback({
 		{ family = "Cica" },
 		{ family = "Cica", assume_emoji_presentation = true },
 	}),
-	font_size = 18.0,
-	color_scheme = "Tokyo Night Moon",
-	use_ime = true,
+	warn_about_missing_glyphs = false,
 	status_update_interval = 1000,
+	use_ime = true,
+	-- Tab Bar Options
+	enable_tab_bar = true,
+	hide_tab_bar_if_only_one_tab = true,
+	show_tab_index_in_tab_bar = false,
+	tab_max_width = 25,
+	-- Padding
+	window_padding = { left = 10, right = 10, top = 10, bottom = 10 },
+	-- Misc
+	adjust_window_size_when_changing_font_size = false,
+	-- Theme
+	colors = {
+		background = COL_BG,
+		foreground = COL_FG,
+		selection_bg = COL_ACCENT,
+		tab_bar = {
+			background = COL_BG,
+			new_tab = {
+				bg_color = COL_BG,
+				fg_color = COL_FG,
+			},
+		},
+	},
+	inactive_pane_hsb = {
+		saturation = 0.85,
+		brightness = 0.85,
+	},
+	hyperlink_rules = {
+		-- Linkify things that look like URLs
+		-- This is actually the default if you don't specify any hyperlink_rules
+		{
+			regex = "\\b\\w+://(?:[\\w.-]+)\\.[a-z]{2,15}\\S*\\b",
+			format = "$0",
+		},
+
+		-- match the URL with a PORT
+		-- such 'http://localhost:3000/index.html'
+		{
+			regex = "\\b\\w+://(?:[\\w.-]+):\\d+\\S*\\b",
+			format = "$0",
+		},
+
+		-- linkify email addresses
+		{
+			regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
+			format = "mailto:$0",
+		},
+
+		-- file:// URI
+		{
+			regex = "\\bfile://\\S*\\b",
+			format = "$0",
+		},
+	},
+	-- keybindings
+	quick_select_alphabet = "colemak",
+	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 },
+	keys = {
+		{ key = "r", mods = "LEADER", action = "ReloadConfiguration" },
+		--
+		{ key = "n", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+		{ key = "|", mods = "LEADER", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+		{ key = "-", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+		--
+		{ key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+		{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+		{ key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+		{ key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+		{ key = "x", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+		--
+		{
+			key = "h",
+			mods = "LEADER|SHIFT",
+			action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }),
+		},
+		{ key = "l", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
+		{ key = "j", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
+		{ key = "k", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
+		--
+		{ key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
+		{ key = "f", mods = "LEADER", action = "QuickSelect" },
+		{ key = "w", mods = "LEADER", action = "ActivateCopyMode" },
+		{ key = "s", mods = "LEADER", action = wezterm.action({ Search = { CaseInSensitiveString = "" } }) },
+		{ key = "PageUp", mods = "NONE", action = wezterm.action({ ScrollByPage = -1 }) },
+		{ key = "PageDown", mods = "NONE", action = wezterm.action({ ScrollByPage = 1 }) },
+		--
+		{ key = "Tab", mods = "LEADER", action = wezterm.action({ ActivateTabRelative = 1 }) },
+		{ key = "Tab", mods = "LEADER|SHIFT", action = wezterm.action({ ActivateTabRelative = -1 }) },
+	},
 }
-
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
--- if wezterm.config_builder then
---	config = wezterm.config_builder()
--- end
-
--- and finally, return the configuration to wezterm
-return config
