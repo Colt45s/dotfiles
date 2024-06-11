6b389db72e8d0a67980b7647cbb27b591f90eb51
Colt45s <kzk0693@gmail.com>
2024-06-11 09:25:54 +0900

    update

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,7 +1,7 @@
 local configs = require "nvchad.configs.lspconfig"
-local on_attach = configs.on_attach
-local on_init = configs.on_init
-local capabilities = configs.capabilities
+-- local on_attach = configs.on_attach
+-- local on_init = configs.on_init
+-- local capabilities = configs.capabilities
 
 local lspconfig = require "lspconfig"
 
@@ -24,16 +24,16 @@ local servers = {
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
+    --    on_attach = on_attach,
+    --    on_init = on_init,
+    --    capabilities = capabilities,
   }
 end
 
 lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
+  --  on_attach = on_attach,
+  --  on_init = on_init,
+  --  capabilities = capabilities,
   commands = {
     OrganizeImports = {
       function()
--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -2,19 +2,19 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
-
+-- copilot
 map("i", "<C-l>", function()
   vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
 end, { replace_keycodes = true, nowait = true, silent = true, expr = true, noremap = true })
 
+-- trouble
 map("n", "<leader>dx", "<cmd> TroubleToggle <cr>", { desc = "Toggle trouble" })
 map("n", "<leader>dd", "<cmd> TroubleToggle document_diagnostics <cr>", { desc = "Document diagnostics" })
 map("n", "<leader>dD", "<cmd> TroubleToggle workspace_diagnostics <cr>", { desc = "Workspace diagnostics" })
 map("n", "<leader>dl", "<cmd> TroubleToggle loclist <cr>", { desc = "Loclist" })
 map("n", "<leader>dq", "<cmd> TroubleToggle quickfix <cr>", { desc = "Quickfix" })
 
+-- spectre
 map("n", "<leader>ss", "<cmd> lua require('spectre').toggle() <cr>", { desc = "Toggle Spectre" })
 map(
   "n",
@@ -30,14 +30,20 @@ map(
 )
 map("v", "<leader>sw", "<cmd> lua require('spectre').open_visual() <cr>", { desc = "Search current word" })
 
-map("n", "<leader>*", "*''cgn")
-
 -- saga
 map("n", "K", "<cmd>Lspsaga hover_doc<CR>")
--- map("n", "gr", "<cmd>Lspsaga lsp_finder<CR>")
--- map("n", "gd", "<cmd>Lspsaga peek_definition<CR>")
--- map("n", "ga", "<cmd>Lspsaga code_action<CR>")
--- map("n", "gn", "<cmd>Lspsaga rename<CR>")
--- map("n", "ge", "<cmd>Lspsaga show_line_diagnostics<CR>")
--- map("n", "[e", "<cmd>Lspsaga diagnostic_jump_next<CR>")
--- map("n", "]e", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
+map("n", "gh", "<cmd>Lspsaga finder<CR>", { desc = "LSP Saga Finder" })
+map("n", "<Leader>ca", "<cmd>Lspsaga code_action<CR>", { desc = "Code Action" })
+map("n", "gr", "<cmd>Lspsaga rename<CR>", { desc = "Rename All Occurrences" })
+map("n", "gp", "<cmd>Lspsaga peek_definition<CR>", { desc = "Peek Definition" })
+map("n", "gd", "<cmd>Lspsaga goto_definition<CR>", { desc = "Go to Definition" })
+map("n", "gt", "<cmd>Lspsaga peek_type_definition<CR>", { desc = "Peek Type Definition" })
+map("n", "<Leader>sl", "<cmd>Lspsaga show_line_diagnostics<CR>", { desc = "Show Line Diagnostics" })
+map("n", "<Leader>sb", "<cmd>Lspsaga show_buf_diagnostics<CR>", { desc = "Show Buffer Diagnostics" })
+map("n", "<Leader>sw", "<cmd>Lspsaga show_workspace_diagnostics<CR>", { desc = "Show Workspace Diagnostics" })
+map("n", "<Leader>sc", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { desc = "Show Cursor Diagnostics" })
+map("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { desc = "Diagnostic Jump Previous" })
+map("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", { desc = "Diagnostic Jump Next" })
+
+-- cgn -> n -> .
+map("n", "<leader>*", "*''cgn")
